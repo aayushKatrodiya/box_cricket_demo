c54f47275d10a7c290476c298e7ce593ccce610c
aayushKatrodiya <aayushkatrodiya8460@gmail.com>
2023-08-26 14:01:55 +0530

    commite

--- lib/box_ui/1splesh_screen.dart
+++ lib/box_ui/1splesh_screen.dart
@@ -17,16 +17,40 @@ class _SpleshScreenState extends State<SpleshScreen> {
           const Image(
             height: 852,
             fit: BoxFit.fill,
-            image: AssetImage("assets/images/box1.jpeg"),
+            image: AssetImage("assets/images/splash_screen_logo2.jpeg"),
           ),
           Center(
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                Container(
+                SizedBox(
                   height: 300,
-                  width: 200,
-                  color: Colors.orange,
+                  width: 220,
+                  // color: Colors.orange,
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset("assets/images/splash screen logo.png"),
+                        const SizedBox(height: 20),
+                        const Text(
+                          "Hello Cricketers",
+                          style: TextStyle(
+                            shadows: [
+                              Shadow(
+                                blurRadius: 1000,
+                                color: Colors.grey,
+                                offset: Offset(0.1, 2),
+                              ),
+                            ],
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 )
               ],
             ),
